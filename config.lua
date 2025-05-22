7a4fc69d38c141b7574cd97667ea8667dd17814e
semyon422 <semyon422@users.noreply.github.com>
2025-05-22 20:28:53 +0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,14 +1,14 @@
 return {
-    github = {
-        repo = "https://github.com/semyon422/soundsphere",
-    },
-    game = {
-        -- repo = "",
-        repo = "https://dl.soundsphere.xyz",
-        api = "https://soundsphere.xyz",
-        multiplayer = "soundsphere.xyz:9000",
-    },
-    osu = {
+	github = {
+		repo = "https://github.com/semyon422/soundsphere",
+	},
+	game = {
+		repo = "https://dl.soundsphere.xyz",
+		api = "https://soundsphere.xyz",
+		multiplayer = "soundsphere.xyz:9000",
+		websocket = "wss://soundsphere.xyz/ws",
+	},
+	osu = {
 		background = "https://assets.ppy.sh/beatmaps/%s/covers/cover.jpg",
 		preview = "https://b.ppy.sh/preview/%s.mp3",
 		download = "https://catboy.best/d/%s",
